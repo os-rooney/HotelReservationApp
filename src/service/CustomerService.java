c4731f6ff5e12edc753fedbbadb36cc1cc69bad0
roosman <dev@ronios.de>
2023-02-20 11:54:07 +0100

    init customer service

---
+++ src/service/CustomerService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.customer.Customer;
+
+import java.util.Collection;
+
+public class CustomerService {
+  public void addCustomer(String email, String firstName, String lastName){
+
+  }
+
+  public Customer getCustomer(String customerEmail){
+    return null;
+  }
+
+  public Collection<Customer> getAllCustomers(){
+    return null;
+  }
+}
