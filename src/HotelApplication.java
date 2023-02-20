0ef067b86cd1d128fb560c975338a1ca61fc1aec
roosman <dev@ronios.de>
2023-02-20 15:37:38 +0100

    rename class

--- src/HotelApplication.java
+++ src/HotelApplication.java
@@ -2,7 +2,7 @@
  * @author Rooney Osman
  * Hotel Reservation Application
  */
-public class Application {
+public class HotelApplication {
   public static void main(String... args){
     MainMenu.startMainMenu();
   }
