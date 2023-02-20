38b9b6a10db25476a1e0a562024a37e057da98bb
roosman <dev@ronios.de>
2023-02-20 11:57:28 +0100

    init customer service as Singleton class, because this is a statefull service

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -3,8 +3,20 @@ package service;
 import model.customer.Customer;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerService {
+
+  private static final CustomerService REFERENCE = new CustomerService();
+  private final Map<String, Customer> customers;
+  private CustomerService(){
+    customers = new HashMap<>();
+  }
+
+  public static CustomerService getInstance(){
+    return REFERENCE;
+  }
   public void addCustomer(String email, String firstName, String lastName){
 
   }
