b8cb0b43aaa4394788abeafd0ab0cfa083bd5652
roosman <dev@ronios.de>
2023-02-20 11:46:43 +0100

    Create Reservation Class

---
+++ src/model/reservation/Reservation.java
@@ -0,0 +1,61 @@
+package model.reservation;
+
+import model.customer.Customer;
+import model.room.IRoom;
+
+import java.util.Date;
+
+public class Reservation {
+  private Customer customer;
+  private IRoom room;
+  private Date checkInDate;
+  private Date checkOutDate;
+
+  public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+    this.customer = customer;
+    this.room = room;
+    this.checkInDate = checkInDate;
+    this.checkOutDate = checkOutDate;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public IRoom getRoom() {
+    return room;
+  }
+
+  public void setRoom(IRoom room) {
+    this.room = room;
+  }
+
+  public Date getCheckInDate() {
+    return checkInDate;
+  }
+
+  public void setCheckInDate(Date checkInDate) {
+    this.checkInDate = checkInDate;
+  }
+
+  public Date getCheckOutDate() {
+    return checkOutDate;
+  }
+
+  public void setCheckOutDate(Date checkOutDate) {
+    this.checkOutDate = checkOutDate;
+  }
+
+  @Override
+  public String toString(){
+    return "Reservation: \n" +
+        "Customer: " + customer + "\n" +
+        "Room: " + room + "\n" +
+        "Check in date: " + checkInDate + "\n" +
+        "Check out date: " + checkOutDate;
+  }
+}
