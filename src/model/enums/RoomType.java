7ef873cf3602856aeed8f32545bfd8f3382d82d8
roosman <dev@ronios.de>
2023-02-20 14:39:51 +0100

    adjust the enum to be able get the type

--- src/model/enums/RoomType.java
+++ src/model/enums/RoomType.java
@@ -1,5 +1,20 @@
 package model.enums;
 
 public enum RoomType {
-  SINGLE, DOUBLE
+  SINGLE("1"), DOUBLE("2");
+
+  public final String type;
+
+  private RoomType(String type) {
+    this.type = type;
+  }
+
+  public static RoomType getType(String type) {
+    for (RoomType roomType : values()) {
+      if (roomType.type.equals(type)) {
+        return roomType;
+      }
+    }
+    throw new IllegalArgumentException();
+  }
 }
