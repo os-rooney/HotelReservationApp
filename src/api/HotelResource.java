fe3e6849c00afb41d7a176c241568859dcdcd0bd
roosman <dev@ronios.de>
2023-02-20 15:32:42 +0100

    complete implementation

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -7,6 +7,7 @@ import service.CustomerService;
 import service.ReservationService;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 
 public class HotelResource {
@@ -26,27 +27,34 @@ public class HotelResource {
   }
 
   public Customer getCustomer(String email){
-    return null;
+    return customerService.getCustomer(email);
   }
 
   public void createACustomer(String email, String firstName, String lastName){
-
+    customerService.addCustomer(email, firstName, lastName);
   }
 
   public IRoom getRoom(String roomNumber){
-    return null;
+    return reservationService.getARoom(roomNumber);
   }
 
   public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate){
-    return null;
+    return reservationService.reserveARoom(getCustomer(customerEmail), room, checkInDate, checkOutDate);
   }
 
   public Collection<Reservation> getCustomersReservations(String customerEmail){
-    return null;
+    Customer customer = getCustomer(customerEmail);
+
+    if (customer == null) {
+      return Collections.emptyList();
+    }
+
+    return reservationService.getCustomersReservation(getCustomer(customerEmail));
   }
 
+
   public Collection<IRoom> findARoom(Date checkIn, Date checkOut){
-    return null;
+    return reservationService.findRooms(checkIn, checkOut);
   }
 
   public Date addDefaultPlusDays(final Date date) {
