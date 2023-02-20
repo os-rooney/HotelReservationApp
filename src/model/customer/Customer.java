7d74f95d26a9879d9c3251498ad1727f14507c13
roosman <dev@ronios.de>
2023-02-20 11:42:19 +0100

    check email format in Customer constructor if it is valid

--- src/model/customer/Customer.java
+++ src/model/customer/Customer.java
@@ -1,11 +1,26 @@
 package model.customer;
 
+import java.util.regex.Pattern;
+
 public class Customer {
   private String firstName;
   private String lastName;
   private String email;
 
+  /**
+   * ^ and $ are anchors that match the start and end of the string, respectively.
+   * [\w-\.]+ matches one or more word characters, hyphens, or dots, which can appear before the @ symbol.
+   * @ matches the literal @ symbol.
+   * ([\w-]+\.)+ matches one or more groups of one or more word characters or hyphens, followed by a dot. This is used to match the domain name, which can consist of multiple parts separated by dots.
+   * [\w-]{2,4} matches the top-level domain, which can consist of two to four word characters or hyphens.
+   */
+  private static final String EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+  private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
   public Customer(String firstName, String lastName, String email){
+    if(EMAIL_PATTERN.matcher(email).matches()){
+      throw new IllegalArgumentException("Invalid email format: " + email);
+    }
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
