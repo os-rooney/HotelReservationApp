3ffb35cba4cb5ccf8d442488ae8c6c93e8aed632
roosman <dev@ronios.de>
2023-02-20 11:14:01 +0100

    create Room type enumeration

---
+++ src/model/IRoom.java
@@ -0,0 +1,10 @@
+package model;
+
+import model.enums.RoomType;
+
+public interface IRoom {
+  public String getRoomNumber();
+  public Double getRoomPrice();
+  public RoomType getRoomType();
+  public boolean isFree();
+}
